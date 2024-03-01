5cac1259becdaea418b0db9770de6d01be6969b3
Tanya-1109 <tanyamohanka@gmail.com>
2024-03-01 11:36:38 +0530

    second commit

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ app =FastAPI()
 
 @app.get('/')
 def index():
-    return 'heyy'
+    return {'data':'blog list'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id : int):
+    #fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id =id
+    return {'data' : { '1' ,'2'}}
